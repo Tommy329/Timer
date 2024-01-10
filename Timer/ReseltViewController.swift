9187ddafb19a6ddf96a09570c7a9beb0ab0798a9
Tommy <hkrt15@icloud.com>
2024-01-10 22:34:59 +0900

    時間をカスタムできるようにしました。バグ修正途中

---
+++ Timer/ReseltViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ReseltViewController.swift
+//  Timer
+//
+//  Created by . HIKARU on 2024/01/10.
+//
+
+import UIKit
+
+class ReseltViewController: UIViewController {
+    
+        @IBOutlet var label : UILabel!
+        var timer : Timer!
+        var countdown : Int = 0
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        @objc func onTimerCalled() {
+            
+            updatelabel()
+            
+            countdown -= 1
+            
+            if countdown < 0 {
+                
+               showstopAlert()
+                
+                timer.invalidate()
+            }}
+        func updatelabel (){
+            
+            let remainingMinutes : Int = countdown / 60
+            let remainingSeconds : Int = countdown % 60
+            label.text = String(format: "%02d:%02d",remainingMinutes,remainingSeconds )
+        }
+        func showstopAlert (){
+            let stopAlert = UIAlertController(title: "タイマーが終了しました。", message: nil, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            stopAlert.addAction(okAction)
+            present(stopAlert,animated: true)
+        }
+            func startTimer(time: Int ) {
+                countdown = time
+                if timer != nil {
+                    timer.invalidate()
+                }
+               timer =  Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerCalled), userInfo: nil, repeats: true)
+                
+                timer.fire()
+        }
+        @IBAction func select30seconds() {
+            countdown = countdown + 30
+            
+        }
+        @IBAction func select60secounds() {
+              countdown = countdown + 60
+        }
+        @IBAction func select300secounds(){
+            countdown = countdown + 300
+        }
+        @IBAction func select600sexounds(){
+            countdown = countdown + 600
+        }
+    @IBAction func start() {
+        startTimer(time: countdown)
+        
+    }
+    @IBAction func back() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func reset() {
+        countdown = 0
+    }
+    }
